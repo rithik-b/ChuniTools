44c1d49519ffb67b9f0b0bd36f42823836307861
PixelBoom <rithikbhatia12@gmail.com>
2023-08-05 23:28:04 -0400

    disabled an eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,6 +25,7 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/no-empty-interface": "off",
   },
 };
 
